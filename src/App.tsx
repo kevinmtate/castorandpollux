0ba9c2191b00bd1263b6fd48e6ed13494e208b9a
Jonathan Silva <jonathan.silva167@gmail.com>
2024-01-13 17:51:46 -0500

    Create routes, pages, and components

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,34 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import HomePage from './pages/HomePage/HomePage';
+import Header from './components/Header/Header';
+import Shows from './pages/Shows/Shows';
+import About from './pages/About/About';
+import Releases from './pages/Releases/Releases';
+import Footer from './components/Hero/Footer/Footer';
+import Contact from './pages/Contact/Contact';
+import PageNotFound from './pages/PageNotFound/PageNotFound';
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<HomePage />} />
+          <Route path='/shows' element={<Shows />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/releases' element={<Releases />} />
+          <Route path='/contact' element={<Contact />} />
+          <Route path='*' element={<PageNotFound />} />
+        </Routes>
+      </BrowserRouter>
+
+      <Footer />
+
     </div>
   );
 }
---
+++ src/pages/Shows/Shows.tsx
@@ -0,0 +1,14 @@
+import TourDates from '../../components/TourDates/TourDates';
+import './Shows.scss';
+
+const Shows = () => {
+    return (
+        <div>
+
+            <TourDates />
+
+        </div>
+    )
+}
+
+export default Shows
