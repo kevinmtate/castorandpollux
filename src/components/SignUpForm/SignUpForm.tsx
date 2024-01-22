4a8f9cfb2d2f17a466d5f9835fe916c32ef9cbb7
Jonathan Silva <jonathan.silva167@gmail.com>
2024-01-21 19:21:34 -0500

    create SignUpForm component + style for mobile, tablet, and desktop.

--- src/components/SignUpForm/SignUpForm.tsx
+++ src/components/SignUpForm/SignUpForm.tsx
@@ -1,11 +1,56 @@
 import './SignUpForm.scss';
+import { useState } from 'react';
+
+type FormData = {
+	firstName: string,
+	lastName: string
+	email: string,
+	zipcode: number
+}
 
 const SignUpForm = () => {
-    return (
-        <div>
+	const [formData, setFormData] = useState({} as any);
 
-        </div>
-    )
+	const handleClick = (e: any) => {
+		e.preventDefault();
+		console.log(e);
+
+	}
+
+	return (
+		<section className='signup'>
+			<h2 className="signup__header">Sign up for C&P's Newsletter</h2>
+
+			<form action="submit" className="signup-form">
+				<fieldset className="signup-form__fieldset">
+					<legend className="signup-form__label">Name</legend>
+
+					<div className="signup-form__top-container">
+						<label htmlFor="firstName" className="signup-form__top-container--sub-label">
+							First Name
+							<input type="text" id="firstName" name="firstName" className="signup-form__top-container--input" required />
+						</label>
+						<label htmlFor="lastName" className="signup-form__top-container--sub-label">
+							Last Name
+							<input type="text" id="lastName" name="lastName" className="signup-form__top-container--input" required />
+						</label>
+					</div>
+
+					<label htmlFor="email" className="signup-form__label">
+						Email
+						<input type="email" id="email" name="email" className="signup-form__input" required />
+					</label>
+					<label htmlFor="zipcode" className="signup-form__label">
+						Zipcode
+						<input type="text" id="zipcode" name="zipcode" className="signup-form__input" required />
+					</label>
+				</fieldset>
+
+				<button className="signup-form--submit-btn" onClick={handleClick} >Submit</button>
+			</form>
+
+		</section>
+	)
 }
 
 export default SignUpForm
