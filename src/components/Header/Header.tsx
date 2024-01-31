b33e7847dc5d3badedbf92b07a4b968312d5a90b
Jonathan Silva <jonathan.silva167@gmail.com>
2024-01-30 22:14:31 -0500

    fix mobile menu, add soundcloud component, ensure consistency in responsive styles, turn X mobile menu button into component, turn Social Media Links in header,mobilemenu, and footer into components, refactor some styling

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,23 +1,42 @@
 import './Header.scss';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import Logo from '../../assets/logos/c&p-logo-full.png';
 import SocialMediaLink from '../SocialMediaLink/SocialMediaLink';
+import MobileMenu from '../MobileMenu/MobileMenu';
 
-const Header = () => {
+import { SocialLink } from '../../App';
+import XButton from '../XButton/XButton';
+
+interface HeaderProps {
+	socialLinks: SocialLink[];
+}
+
+const Header: React.FC<HeaderProps> = ({ socialLinks }) => {
 	const [mobileMenu, setMobileMenu] = useState(false);
+	const [hideMenu, setHideMenu] = useState(true);
+	const [isScrollingDisabled, setScrollingDisabled] = useState(false);
 
 	const handleMobileMenuToggleClick = () => {
 		if (mobileMenu === true) {
 			setMobileMenu(false);
+			setScrollingDisabled(false);
+			setTimeout(() => {
+				setHideMenu(true);
+			}, 500);
 		} else {
+			setHideMenu(false);
 			setMobileMenu(true);
+			window.scrollTo(0, 0);
+			setScrollingDisabled(true);
+
 		}
 	}
 
-	const handleMobileMenuClick = () => {
-		setMobileMenu(false);
-	}
+	useEffect(() => {
+		document.body.style.overflow = isScrollingDisabled ? 'hidden' : 'auto';
+	}, [isScrollingDisabled]);
+
 
 	return (
 		<header className="header">
@@ -27,9 +46,6 @@ const Header = () => {
 					<li className="header-nav__main-nav--link">
 						<Link to="/" className='' >Home</Link>
 					</li>
-					{/* <li className="header-nav__main-nav--link">
-						<Link to="/about" className='' >About</Link>
-					</li> */}
 					<li className="header-nav__main-nav--link">
 						<Link to="/releases" className='' >Releases</Link>
 					</li>
@@ -46,107 +62,24 @@ const Header = () => {
 					<img className='header-nav__logo--img' src={Logo} alt='Castor & Pollux Logo' />
 				</Link>
 
-				<button className="mobile__nav-button" onClick={handleMobileMenuToggleClick}>
-					<div className={mobileMenu === true ? "mobile__nav-button--middle-line animate-top" : mobileMenu === false ? "mobile__nav-button--middle-line reverse-top" : "mobile__nav-button--middle-line"}></div>
-					<div className={mobileMenu === true ? "mobile__nav-button--first-line animate-middle" : mobileMenu === false ? "mobile__nav-button--first-line reverse-middle" : "mobile__nav-button--first-line"}></div>
-					<div className={mobileMenu === true ? "mobile__nav-button--last-line animate-bottom" : mobileMenu === false ? "mobile__nav-button--last-line reverse-bottom" : "mobile__nav-button--last-line"}></div>
-				</button>
-
+				<XButton mobileMenu={mobileMenu} handleMobileMenuToggleClick={handleMobileMenuToggleClick} />
 
 				<ul className="header-nav__social-nav">
-					<SocialMediaLink
-						site='Spotify'
-						siteLink='https://open.spotify.com/artist/6fHUILCpBvfSHB48AEciPG?si=SQQEFDC-RrKzxtKizT5HHg&nd=1&dlsi=a21d5c44637f4ab0'
-					/>
-
-					<SocialMediaLink
-						site='Soundcloud'
-						siteLink='https://soundcloud.com/castorandpollux_music'
-					/>
-
-					<SocialMediaLink
-						site='Facebook'
-						siteLink='https://www.facebook.com/candpmusic/'
-					/>
-
-					<SocialMediaLink
-						site='Instagram'
-						siteLink='https://www.instagram.com/castorandpolluxmusic/'
-					/>
-
-					<SocialMediaLink
-						site='Twitter'
-						siteLink='https://twitter.com/candpmusic'
-					/>
-
-					<SocialMediaLink
-						site='Youtube'
-						siteLink='https://www.youtube.com/channel/UCp2BJQiRN36uNGac76eAS6A?view_as=subscriber'
-					/>
-
-					<SocialMediaLink
-						site='Twitch'
-						siteLink='https://www.twitch.tv/castorandpolluxmusic'
-					/>
+					{socialLinks.map((socialLink, key) => {
+						return (
+							<SocialMediaLink
+								key={key}
+								site={socialLink.site}
+								siteLink={socialLink.siteLink}
+							/>
+						)
+					})}
 				</ul>
 			</nav>
 
-			<aside className={mobileMenu === true ? "mobile-menu fade-in" : mobileMenu === false ? "mobile-menu fade-out" : "mobile-menu"}>
-				<ul className="header-nav__main-nav-mobile">
-					<li className="header-nav__main-nav-mobile--link">
-						<Link to="/" className='' onClick={handleMobileMenuClick}  >Home</Link>
-					</li>
-					<li className="header-nav__main-nav-mobile--link">
-						<Link to="/about" className='' onClick={handleMobileMenuClick} >About</Link>
-					</li>
-					<li className="header-nav__main-nav-mobile--link">
-						<Link to="/releases" className='' onClick={handleMobileMenuClick} >Releases</Link>
-					</li>
-					<li className="header-nav__main-nav-mobile--link">
-						<Link to="/shows" className='' onClick={handleMobileMenuClick} >Shows</Link>
-					</li>
-					<li className="header-nav__main-nav-mobile--link">
-						<Link to="/contact" className='' onClick={handleMobileMenuClick} >Contact Us</Link>
-					</li>
-				</ul>
-
-				<ul className="header-nav__social-nav-mobile">
-					<SocialMediaLink
-						site='Spotify'
-						siteLink='https://open.spotify.com/artist/6fHUILCpBvfSHB48AEciPG?si=SQQEFDC-RrKzxtKizT5HHg&nd=1&dlsi=a21d5c44637f4ab0'
-					/>
-
-					<SocialMediaLink
-						site='Soundcloud'
-						siteLink='https://soundcloud.com/castorandpollux_music'
-					/>
-
-					<SocialMediaLink
-						site='Facebook'
-						siteLink='https://www.facebook.com/candpmusic/'
-					/>
-
-					<SocialMediaLink
-						site='Instagram'
-						siteLink='https://www.instagram.com/castorandpolluxmusic/'
-					/>
-
-					<SocialMediaLink
-						site='Twitter'
-						siteLink='https://twitter.com/candpmusic'
-					/>
-
-					<SocialMediaLink
-						site='Youtube'
-						siteLink='https://www.youtube.com/channel/UCp2BJQiRN36uNGac76eAS6A?view_as=subscriber'
-					/>
-
-					<SocialMediaLink
-						site='Twitch'
-						siteLink='https://www.twitch.tv/castorandpolluxmusic'
-					/>
-				</ul>
-			</aside>
+			{hideMenu === false && (
+				<MobileMenu mobileMenu={mobileMenu} setMobileMenu={setMobileMenu} socialLinks={socialLinks} />
+			)}
 
 
 		</header>
--- src/components/RecentReleases/RecentReleases.tsx
+++ src/components/RecentReleases/RecentReleases.tsx
@@ -1,6 +1,17 @@
+import { useEffect, useState } from 'react';
 import './RecentReleases.scss';
 
 const RecentReleases = () => {
+	const [showSoundcloud, setShowSoundcloud] = useState<boolean | null>(null);
+
+	const handleClick = () => {
+		setShowSoundcloud(true);
+	}
+
+	const handleClose = () => {
+		setShowSoundcloud(false);
+	}
+
 	return (
 		<section className='recent'>
 
@@ -8,54 +19,94 @@ const RecentReleases = () => {
 				Recent Releases
 			</h2>
 			<div className="recent__releases">
-				<iframe
-					className='recent__releases--card'
-					style={{ borderRadius: "12px" }}
-					src="https://open.spotify.com/embed/track/3guu1NVq5cglm8yzIbUqAp?utm_source=generator"
-					width="100%"
-					height="352"
-					frameBorder="0"
-					allowFullScreen={true}
-					allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
-					loading="lazy">
-				</iframe>
-				<iframe
-					className='recent__releases--card'
-					style={{ borderRadius: "12px" }}
-					src="https://open.spotify.com/embed/track/5cexuf9cOVmteecGvt2ljY?utm_source=generator"
-					width="100%"
-					height="352"
-					frameBorder="0"
-					allowFullScreen={true}
-					allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
-					loading="lazy">
-				</iframe>
-				<iframe
-					className='recent__releases--card'
-					style={{ borderRadius: "12px" }}
-					src="https://open.spotify.com/embed/track/0wCO8Ei3hn5lYbG3m6PIOU?utm_source=generator"
-					width="100%"
-					height="352"
-					frameBorder="0"
-					allowFullScreen={true}
-					allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
-					loading="lazy">
-				</iframe>
-				<iframe
-					className='recent__releases--card'
-					style={{ borderRadius: "12px" }}
-					src="https://open.spotify.com/embed/track/4C9uiUGtAuaqwOyDBlENWf?utm_source=generator"
-					width="100%"
-					height="352"
-					frameBorder="0"
-					allowFullScreen={true}
-					allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
-					loading="lazy">
-				</iframe>
+				<div className='recent__releases-container'>
+					<iframe
+						className='recent__releases--card'
+						style={{ borderRadius: "12px" }}
+						src="https://open.spotify.com/embed/track/3guu1NVq5cglm8yzIbUqAp?utm_source=generator"
+						width="100%"
+						height="352"
+						frameBorder="0"
+						allowFullScreen={true}
+						allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+						loading="lazy"
+					>
+					</iframe>
+					<div className='recent__releases--click-area' onClick={handleClick} />
+				</div>
+				<div className='recent__releases-container'>
+					<iframe
+						className='recent__releases--card'
+						style={{ borderRadius: "12px" }}
+						src="https://open.spotify.com/embed/track/5cexuf9cOVmteecGvt2ljY?utm_source=generator"
+						width="100%"
+						height="352"
+						frameBorder="0"
+						allowFullScreen={true}
+						allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+						loading="lazy"
+					>
+					</iframe>
+					<div className='recent__releases--click-area' onClick={handleClick} />
+				</div>
+				<div className='recent__releases-container'>
+					<iframe
+						className='recent__releases--card'
+						style={{ borderRadius: "12px" }}
+						src="https://open.spotify.com/embed/track/0wCO8Ei3hn5lYbG3m6PIOU?utm_source=generator"
+						width="100%"
+						height="352"
+						frameBorder="0"
+						allowFullScreen={true}
+						allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+						loading="lazy"
+					>
+					</iframe>
+					<div className='recent__releases--click-area' onClick={handleClick} />
+				</div>
+				<div className='recent__releases-container'>
+					<iframe
+						className='recent__releases--card'
+						style={{ borderRadius: "12px" }}
+						src="https://open.spotify.com/embed/track/2nibAHzEdndxBW3cptLffC?utm_source=generator"
+						width="100%"
+						height="352"
+						frameBorder="0"
+						allowFullScreen={true}
+						allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+						loading="lazy"
+					>
+					</iframe>
+					<div className='recent__releases--click-area' onClick={handleClick} />
+				</div>
 			</div>
-
-		</section>
+			<a className={`soundcloud__button--open ${showSoundcloud ? "button-hide" : "button-show"}`} onClick={handleClick}>+</a>
+			<aside className={`soundcloud ${showSoundcloud ? "ani-expand" : "ani-contract"}`}>
+				<iframe
+					width="100%"
+					height="450"
+					scrolling="no"
+					frameBorder="no"
+					allow="autoplay"
+					src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/955313764&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true">
+				</iframe>
+				<div style={{
+					fontSize: "10px",
+					color: "#cccccc",
+					lineBreak: "anywhere",
+					wordBreak: "normal",
+					overflow: "hidden",
+					whiteSpace: "nowrap",
+					textOverflow: "ellipsis",
+					fontFamily: " Interstate,Lucida Grande,Lucida Sans Unicode,Lucida Sans,Garuda,Verdana,Tahoma,sans-serif",
+					fontWeight: 100
+				}}>
+				</div>
+				<a className={`soundcloud__button ${showSoundcloud ? "button-show" : "button-hide"}`} onClick={handleClose}>click to close</a>
+			</aside>
+		</section >
 	)
 }
 
 export default RecentReleases
+
