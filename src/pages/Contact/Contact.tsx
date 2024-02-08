50f2306e10d48013490c44ff9397a0126f2ef97f
Jonathan Silva <jonathan.silva167@gmail.com>
2024-02-07 19:00:53 -0500

    Add font styling + responsiveness styling for all devices

--- src/pages/Contact/Contact.tsx
+++ src/pages/Contact/Contact.tsx
@@ -10,16 +10,25 @@ const Contact = () => {
 			<section className='contact'>
 				<h2 className="contact__header">Contact</h2>
 				<div className="contact__container">
-					<aside className="contact__container--column">
-						<h3 className="contact__container--column__header">MGMT</h3>
-						<p className="contact__container--column__text">Bookings/General Inquiries</p>
-						<a href="mgmt@castorandpolluxofficial.com">Send Email</a>
+					<aside className="contact__container-column">
+						<h3 className="contact__container-column--header">Castor & Pollux</h3>
+						<a
+							href="castorandpollux@castorandpolluxofficial.com"
+							className="contact__container-column--link"
+						>
+							castorandpollux@castorandpolluxofficial.com
+						</a>
 					</aside>
-					<img src={shortLogo} alt="" className="contact__container--	logo" />
-					<aside className="contact__container--column">
-						<h3 className="contact__container--column__header">MGMT</h3>
-						<p className="contact__container--column__text">Bookings/General Inquiries</p>
-						<a href="mgmt@castorandpolluxofficial.com">Send Email</a>
+					<img src={shortLogo} alt="" className="contact__container--logo" />
+					<aside className="contact__container-column">
+						<h3 className="contact__container-column--header">MGMT</h3>
+						<p className="contact__container-column--text">Bookings/General Inquiries</p>
+						<a
+							href="mgmt@castorandpolluxofficial.com"
+							className="contact__container-column--link"
+						>
+							mgmt@castorandpolluxofficial.com
+						</a>
 					</aside>
 				</div>
 
