eb439b07eeca318b18949fd04b5679c7e15e62c5
Jonathan Silva <jonathan.silva167@gmail.com>
2024-05-30 16:04:05 -0400

    convert new releases to JSON data and map function

--- src/pages/Releases/Releases.tsx
+++ src/pages/Releases/Releases.tsx
@@ -1,13 +1,10 @@
 import RecentReleases from '../../components/RecentReleases/RecentReleases'
 import './Releases.scss'
 
-const Releases = () => {
-    return (
-        <div>
-            <RecentReleases />
-
-        </div>
-    )
+export default function Releases() {
+	return (
+		<div>
+			<RecentReleases />
+		</div>
+	)
 }
-
-export default Releases
